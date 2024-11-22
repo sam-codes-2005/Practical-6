133ddc431097faf66bfd4ca517c6759a5e25a1bd
Saksham <apple@apples-MacBook-Pro.local>
2024-11-22 23:55:03 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # Practical-6
+=======
+# labwork 7: Music Box
+
+This is the starter code for labwork 7: Music Box. Please [check out the spec for an explanation of the starter files.
+>>>>>>> ee9aef3 (initial commit)
---
+++ play-button.js
@@ -0,0 +1,10 @@
+// This class will represent the play button in the MusicScreen. Clicking on
+// it toggles audio playback.
+//
+// See labwork 7 writeup for more hints and details.
+class PlayButton {
+  constructor() {
+    // TODO(you): Implement the constructor and add fields as necessary.
+  }
+  // TODO(you): Add methods as necessary.
+}
